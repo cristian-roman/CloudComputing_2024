15f71c773883ecf2eb32bb24717747fe7622dc39
Cristian Roman <romancristianroman@yahoo.com>
2024-04-24 15:03:46 +0300

    residue changes

--- SecondHomework/calendar/Models/WeatherModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Calendar.Models;
-
-public class WeatherModel
-{
-    public DateTime CurrentTime { get; set; }
-    public float Temperature { get; set; }
-    public string ConditionText { get; set; }
-    public string ImageUrl { get; set; }
-}
