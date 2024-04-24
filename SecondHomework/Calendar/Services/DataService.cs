447d677140fe60711d4b5f1a5b48e91580c53ab1
Cristian Roman <romancristianroman@yahoo.com>
2024-04-24 15:06:45 +0300

    added left files

---
+++ SecondHomework/Calendar/Services/DataService.cs
@@ -0,0 +1,28 @@
+﻿using Calendar.Models;
+
+namespace Calendar.Services;
+
+public class DataService
+{
+    public string? Ip { get; set; }
+    public string? Location { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public List<EventModel> Events { get; set; } = [];
+
+    public event EventHandler DataFetched;
+
+    public virtual void OnDataFetched(EventArgs e)
+    {
+        DataFetched?.Invoke(this, e);
+    }
+
+    public event EventHandler LocalTimeChanged;
+
+    public virtual void OnLocalTimeChanged(EventArgs e)
+    {
+        LocalTimeChanged?.Invoke(this, e);
+    }
+
+}
